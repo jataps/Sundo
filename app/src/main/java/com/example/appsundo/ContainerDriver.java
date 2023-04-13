b49ef08cc1b22912ff24838caed0684755eb2a8a
JL <jeyelbiti@gmail.com>
2023-04-14 01:04:37 +0800

    Sample edit new-branch

--- app/src/main/java/com/example/appsundo/ContainerDriver.java
+++ app/src/main/java/com/example/appsundo/ContainerDriver.java
@@ -14,6 +14,8 @@ import com.example.appsundo.databinding.ActivityContainerDriverBinding;
 public class ContainerDriver extends AppCompatActivity {
 
     ActivityContainerDriverBinding binding;
+
+    int x; //added for sample
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
