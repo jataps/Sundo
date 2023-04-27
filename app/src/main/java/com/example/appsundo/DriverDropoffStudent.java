fe407a9559d500f7d1b01d1816aba8a0d1b25c86
Gigixxx1 <ortega.gbunag@gmail.com>
2023-04-28 03:31:05 +0800

    update
    fillupform and Ddropoff

---
+++ app/src/main/java/com/example/appsundo/DriverDropoffStudent.java
@@ -0,0 +1,185 @@
+package com.example.appsundo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class DriverDropoffStudent extends AppCompatActivity implements RecyclerViewInterface {
+
+    RecyclerView studentList;
+    DatabaseReference mRef;
+    CustomStudentAdapter adapter;
+    ArrayList<User> list;
+    MaterialButton btnAddStudent;
+
+    TextView txtFname;
+    TextView txtLname;
+    TextView txtEmerName;
+    TextView txtEmerNumber;
+    TextView txtContactNumber;
+    TextView txtProvince;
+    TextView txtCity;
+    TextView txtBarangay;
+    TextView txtStAddress;
+    TextView txtUid;
+
+    User userDriver;
+
+    String firstName, lastName, emerName, emerNumber, contactNumber, province, city, barangay, stAddress, uidStudent, accountCode, infoId;
+
+    /*Code format copied from DriverPickupStudent*/
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_dropoff_student);
+
+
+
+        studentList = findViewById(R.id.studentList);
+        btnAddStudent = findViewById(R.id.btnAddStudent);
+
+        txtFname = findViewById(R.id.txtFName);
+        txtLname = findViewById(R.id.txtLName);
+        txtEmerName = findViewById(R.id.textEmerName);
+        txtEmerNumber = findViewById(R.id.textEmerNumber);
+        txtContactNumber = findViewById(R.id.txtContactNumber);
+        txtProvince = findViewById(R.id.textAddress);
+        txtStAddress = findViewById(R.id.textStAddress);
+
+        uidStudent = getIntent().getStringExtra("UID");
+        accountCode = getIntent().getStringExtra("ACCOUNT_CODE");
+        lastName = getIntent().getStringExtra("LAST_NAME");
+        firstName = getIntent().getStringExtra("FIRST_NAME");
+        contactNumber = getIntent().getStringExtra("CONTACT_NUMBER");
+        emerName = getIntent().getStringExtra("EMERGENCY_NAME");
+        emerNumber = getIntent().getStringExtra("EMERGENCY_NUMBER");
+        province = getIntent().getStringExtra("PROVINCE");
+        city = getIntent().getStringExtra("CITY");
+        barangay = getIntent().getStringExtra("BARANGAY");
+        stAddress = getIntent().getStringExtra("ST_ADDRESS");
+        infoId = getIntent().getStringExtra("INFO_REF");
+
+        txtFname.setText(firstName);
+        txtLname.setText(lastName);
+        txtEmerName.setText(emerName);
+        txtEmerNumber.setText(emerNumber);
+        txtContactNumber.setText(contactNumber);
+        txtProvince.setText(barangay + " | " + city + " | " + province);
+        txtStAddress.setText(stAddress);
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+
+        btnAddStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+                DatabaseReference driverRef = ref.child("USERS").child("DRIVER").child(uid).child("ASSIGNED_STUDENT").child(accountCode);
+
+                driverRef.child("UID").setValue(uidStudent);
+                driverRef.child("firstName").setValue(firstName);
+                driverRef.child("lastName").setValue(lastName);
+                driverRef.child("ADDRESS/province").setValue(province);
+                driverRef.child("ADDRESS/city").setValue(city);
+                driverRef.child("ADDRESS/barangay").setValue(barangay);
+                driverRef.child("ADDRESS/streetAddress").setValue(barangay);
+                driverRef.child("contactNumber").setValue(contactNumber);
+
+                getDriverInfo(ref, uid);
+
+            }
+        });
+    }
+
+    public void getDriverInfo(DatabaseReference dbRef, String uid) {
+
+        DatabaseReference infoIdRef;
+
+        infoIdRef = dbRef.child("USERS").child("DRIVER").child(uid).child("INFO_ID");
+        infoIdRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    // User with the given UID exists in DRIVER
+
+                    String infoID = String.valueOf(snapshot.getValue());
+
+                    DatabaseReference profileRef = dbRef.child("USER_INFORMATION").child("DRIVER").child(infoID);
+
+                    profileRef.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if (snapshot.exists()) {
+                                // User with the given UID exists in DRIVER
+
+                                //studentList.clear();
+                                userDriver = snapshot.getValue(User.class);
+
+                                DatabaseReference studentRef = dbRef.child("USERS").child("STUDENT").child(uidStudent).child("PICKUP_STUDENTS").child(userDriver.getAccountCode());
+
+                                studentRef.child("UID").setValue(userDriver.getUid());
+                                studentRef.child("firstName").setValue(userDriver.getFirstName());
+                                studentRef.child("lastName").setValue(userDriver.getLastName());
+                                studentRef.child("contactNumber").setValue(userDriver.getContactNumber());
+                                studentRef.child("province").setValue(userDriver.getADDRESS().getProvince());
+                                studentRef.child("city").setValue(userDriver.getADDRESS().getCity());
+                                studentRef.child("barangay").setValue(userDriver.getADDRESS().getBarangay());
+
+                                dbRef.child("USER_INFORMATION").child("STUDENT").child(infoId).child("DRIVER_ASSIGNED").setValue(userDriver.getAccountCode());
+
+                                Toast.makeText(DriverDropoffStudent.this, "Student Successfully Picked Up", Toast.LENGTH_SHORT).show();
+
+                                Intent intent = new Intent(getApplicationContext(),ContainerDriver.class);
+                                intent.putExtra("fragment_to_display","fragment_service");
+                                intent.putExtra("service_to_display","add_student");
+                                startActivity(intent);
+                                finish();
+
+                            } else {
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onItemClick(int position) {
+
+    }
+}
--- app/src/main/java/com/example/appsundo/DriverFragmentDropOff.java
+++ app/src/main/java/com/example/appsundo/DriverFragmentDropOff.java
@@ -1,22 +1,103 @@
 package com.example.appsundo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class DriverFragmentDropOff extends Fragment {
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DriverFragmentDropOff extends Fragment implements RecyclerViewInterface {
+
+    RecyclerView dropOffStudentList;
+    DatabaseReference mRef;
+    CustomStudentAdapter adapter;
+    ArrayList<User> list;
+    User student;
+
+    MaterialButton addStudentBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_driver_drop_off, container, false);
 
+        dropOffStudentList = view.findViewById(R.id.dropOffStudentList);
+
+        String uidDriver = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        mRef = FirebaseDatabase.getInstance().getReference().child("USERS").child("DRIVER").child(uidDriver).child("ASSIGNED_STUDENT");
+
+        dropOffStudentList.setHasFixedSize(true);
+        dropOffStudentList.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        list = new ArrayList<>();
+        adapter = new CustomStudentAdapter(getActivity(), list, this);
+        dropOffStudentList.setAdapter(adapter);
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                list.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+
+                    student = dataSnapshot.getValue(User.class);
+                    list.add(student);
+
+                    Collections.sort(list);
+                }
+
+                adapter.notifyDataSetChanged();
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         // Inflate the layout for this fragment
         return view;
     }
+
+    @Override
+    public void onItemClick(int position) {
+
+        Intent intent = new Intent(getActivity(), DriverDropoffStudent.class);
+
+        intent.putExtra("fragment_to_display","fragment_service");
+        intent.putExtra("INFO_REF", list.get(position).getReferenceID());
+        intent.putExtra("UID", list.get(position).getUid());
+        intent.putExtra("LAST_NAME", list.get(position).getLastName());
+        intent.putExtra("FIRST_NAME", list.get(position).getFirstName());
+        intent.putExtra("CONTACT_NUMBER", list.get(position).getContactNumber());
+        intent.putExtra("EMERGENCY_NAME", list.get(position).getEmergencyName());
+        intent.putExtra("EMERGENCY_NUMBER", list.get(position).getEmergencyNumber());
+        intent.putExtra("PROVINCE", list.get(position).getADDRESS().getProvince());
+        intent.putExtra("CITY", list.get(position).getADDRESS().getCity());
+        intent.putExtra("BARANGAY", list.get(position).getADDRESS().getBarangay());
+        intent.putExtra("ST_ADDRESS", list.get(position).getADDRESS().getStreetAddress());
+        intent.putExtra("ACCOUNT_CODE", list.get(position).getAccountCode());
+
+        startActivity(intent);
+
+    }
+
 }
